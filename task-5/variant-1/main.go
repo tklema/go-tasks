916a864c644c2fc401dfca8e6e30c6e42dd5f23b
Klementev_Timofei <367285@niuitmo.ru>
2025-08-26 15:23:15 +0300

    task-5

---
+++ task-5/variant-1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	seconds := 2
+
+	ch := make(chan int)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case elem := <-ch:
+				fmt.Printf("Read: %d\n", elem)
+			case <-ctx.Done():
+				fmt.Println("End of reading")
+				return
+			}
+		}
+	}()
+
+	timeout := time.After(time.Duration(seconds) * time.Second)
+
+	elem := 1
+	for {
+		select {
+		case ch <- elem:
+			elem++
+		case <-timeout:
+			cancel()
+			wg.Wait()
+			close(ch)
+			fmt.Println("End of work")
+			return
+		}
+	}
+}
