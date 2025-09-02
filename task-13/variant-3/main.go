8b6eac57db2f1b8c877af1c2ed49fb310e997914
Klementev_Timofei <367285@niuitmo.ru>
2025-09-02 17:59:12 +0300

    task-13

---
+++ task-13/variant-3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := 7
+	b := 25
+
+	fmt.Printf("a: %d, b: %d\n", a, b)
+
+	a = a ^ b
+	b = a ^ b
+	a = a ^ b
+
+	fmt.Printf("a: %d, b: %d\n", a, b)
+}
