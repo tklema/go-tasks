3eb3dc720651228ae584db6b0b05eaaeffbcce27
Klementev_Timofei <367285@niuitmo.ru>
2025-09-05 18:01:37 +0300

    task-23

---
+++ task-23/append/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func remove(arr []int, index int) []int {
+	return append(arr[:index], arr[index+1:]...)
+}
+
+func main() {
+
+	ind := 2
+
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(arr)
+	for range len(arr) - ind {
+		arr = remove(arr, ind)
+		fmt.Println(arr)
+	}
+}
